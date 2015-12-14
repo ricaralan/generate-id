3a43bb279439eb973209dc59d2ad854b2e9025ca
Alan <richard.alan.oli@gmail.com>
2015-12-13 23:58:51 -0600

    I modified structure module to programming based on prototypes Version 1.6.1 to 2.0.1

--- index.js
+++ index.js
@@ -2,55 +2,69 @@
 * This module help to generate ids
 *
 * @author Alan Olivares
-* @version 1.6.1
+* @version 2.0.1
 */
-module.exports = function () {
 
-  var toGenerate = {
+'use strict'
+
+var GenerateId = function() {
+  this._toGenerate = {
     down    : "abcdefghijklmnopqrstuvwxyz",
     upper   : "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     numbers : "0123456789"
   };
+};
 
-  this.generate = function(optionsIn) {
-    var optionsGenerate = getArrayToGenerate(optionsIn.include);
-    requiredLength = getRequiredLength(optionsIn);
-    word = "";
-    for(var i = 0; i < requiredLength; i++) {
-      keys = Object.keys(optionsGenerate);
-      option = optionsGenerate[keys[getRandom(0, keys.length )]];
-      word += option[getRandom(0, option.length -1)];
-    }
-    return getWordAdd(word, (optionsIn instanceof Object) ? optionsIn : null );
-  };
+/**
+* generate() generates a random IDs
+*
+* @param  {JSON | String} optionsIn
+* @return {String} generated id
+*/
+GenerateId.prototype.generate = function(optionsIn) {
+  var optionsGenerate = this._getArrayToGenerate(optionsIn.include);
+  var requiredLength = this._getRequiredLength(optionsIn);
+  var word = "";
+  var keys = null;
+  var option = null;
 
-  function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  };
+  for(var i = 0; i < requiredLength; i++) {
+    keys = Object.keys(optionsGenerate);
+    option = optionsGenerate[keys[this._getRandom(0, keys.length )]];
+    word += option[this._getRandom(0, option.length -1)];
+  }
 
-  function getArrayToGenerate(options) {
-    if(options && options instanceof Object && options.length) {
-      array = [];
-      for(i in options) {
-        if(toGenerate[options[i]]) {
-          array.push(toGenerate[options[i]]);
-        }
+  return this._getWordAdd(word, (optionsIn instanceof Object) ? optionsIn : null );
+};
+
+GenerateId.prototype._getRandom = function(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+GenerateId.prototype._getArrayToGenerate = function(options) {
+  if(options && options instanceof Object && options.length) {
+    var array = [];
+    var i = null;
+    for(i in options) {
+      if(this._toGenerate[options[i]]) {
+        array.push(this._toGenerate[options[i]]);
       }
-      return array;
     }
-    return toGenerate;
+    return array;
   }
-
-  function getWordAdd(word, options) {
-    if(options && options.add) {
-      word  = (options.add.before) ? options.add.before + word : word;
-      word += (options.add.after)  ? options.add.after : "";
-    }
-    return word;
-  }
-
-  function getRequiredLength(options){
-    return (options instanceof Object && options.length) ? options.length : options;
-  }
-
+  return this._toGenerate;
 }
+
+GenerateId.prototype._getWordAdd = function(word, options) {
+  if(options && options.add) {
+    word  = (options.add.before) ? options.add.before + word : word;
+    word += (options.add.after)  ? options.add.after : "";
+  }
+  return word;
+}
+
+GenerateId.prototype._getRequiredLength = function(options){
+  return (options instanceof Object && options.length) ? options.length : options;
+}
+
+module.exports = GenerateId;
